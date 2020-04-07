cfa3deedf4b398f092c1233e028a939e5a0e8fb4
Marcelo Bini <marcelobini@gmail.com>
2020-04-07 19:52:09 +0000

    Done.

--- hashketball.rb
+++ hashketball.rb
@@ -3,7 +3,7 @@ def game_hash
   {
     home: {
       team_name: "Brooklyn Nets",
-      colors: ["Black, White"],
+      colors: ["Black", "White"],
       players: [
         {player_name:"Alan Anderson",number:0,shoe:16,points:22,rebounds:12,assists:12,steals:3,blocks:1,slam_dunks:1},
         {player_name:"Reggie Evans",number:30,shoe:14,points:12,rebounds:12,assists:12,steals:12,blocks:12,slam_dunks:7},
@@ -14,7 +14,7 @@ def game_hash
   },
     away: {
       team_name:"Charlotte Hornets",
-      colors: ["Turquoise, Purple"],
+      colors: ["Turquoise", "Purple"],
       players: [
         {player_name:"Jeff Adrien",number:4,shoe:18,points:10,rebounds:1,assists:1,steals:2,blocks:7,slam_dunks:2},
        {player_name:"Bismack Biyombo",number:0,shoe:16,points:12,rebounds:4,assists:7,steals:22,blocks:15,slam_dunks:10},
@@ -36,4 +36,78 @@ def num_points_scored(player_name)
       end
     end
   end
+end
+
+def shoe_size(player_name)
+  game_hash.each do |_place, team|
+    team.each do |attribute, data|
+      next unless attribute == :players
+
+      data.each do |player|
+        return player[:shoe] if player[:player_name] == player_name
+      end
+    end
+  end
+end
+
+def team_colors(team_name)
+  game_hash.each do |place, team|
+    return game_hash[place][:colors] if team[:team_name] == team_name
+  end
+end
+
+def team_names
+  game_hash.collect do |_place, team|
+    team[:team_name]
+  end
+end
+
+def player_numbers(team_name)
+  nums = []
+  game_hash.each do |_place, team|
+    next unless team[:team_name] == team_name
+
+    team.each do |attribute, data|
+      next unless attribute == :players
+
+      data.each do |data|
+        nums << data[:number]
+      end
+    end
+  end
+  nums
+end
+
+def player_stats(sought_player_name)
+  new_hash = {}
+  game_hash.collect do |place, team|
+    team.each do |attribute, _data|
+      next unless attribute == :players
+
+      game_hash[place][attribute].each do |player|
+        next unless player[:player_name] == sought_player_name
+
+        new_hash = player.delete_if do |k, _v|
+          k == :player_name
+        end
+      end
+    end
+  end
+  new_hash
+end
+
+def big_shoe_rebounds
+  biggest_shoe = 0
+  num_rebounds = 0
+
+  game_hash.each do |_team, game_data|
+    game_data[:players].each do |player|
+      if player[:shoe] > biggest_shoe
+        biggest_shoe = player[:shoe]
+        num_rebounds = player[:rebounds]
+      end
+    end
+  end
+
+  num_rebounds
 end
